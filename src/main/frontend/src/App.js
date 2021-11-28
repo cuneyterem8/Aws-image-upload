c13301aa7d5a11bee2fd0dc45954ab96dab88d54
Cüneyt Erem <s6cuerem@uni-bonn.de>
2021-11-28 22:42:41 +0100

    react dropzone and file upload

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect, useCallback} from "react";
+import axios from "axios";
+import {useDropzone} from 'react-dropzone'
+
+const UserProfiles = () => {
+  const [userProfiles, setUserProfiles] = useState([]);
+
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
+      console.log(res);
+      setUserProfiles(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchUserProfiles();
+  }, []);
+
+  return userProfiles.map((userProfile, index) => {
+    return (<div key={index}>
+      <br/>
+      <br/>
+      <h1>{userProfile.username}</h1>
+      <p>{userProfile.userProfileId}</p>
+      <Dropzone {...userProfile}/>
+      <br/>
+    </div>)
+  });
+};
+
+function Dropzone({userProfileId}) {
+  const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+    console.log(file);
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    axios.post(
+        "http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload",
+        formData,
+        {
+          headers: {"Content-Type": "multipart/form-data"}
+        }).then(() => {
+          console.log("file uploaded");
+    }).catch(err => {
+      console.log(err);
+    });
+  }, []);
+
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
+
+  return (
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+              <p>Drop the files here ...</p> :
+              <p>Drag 'n' drop or click for profile image</p>
+        }
+      </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and Hello world
-        </a>
-      </header>
+      <UserProfiles/>
     </div>
   );
 }
